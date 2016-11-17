2fc2fce0320d0366fd8836e0acf20b15477902a0
Plugin project added <deepakkumar1984@gmail.com>
2016-11-17 15:02:48 +1030

    Storage cleaner project added

---
+++ BDK.XrmToolBox/BDK.XrmToolBox.StorageCleaner/PluginControl.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using XrmToolBox.Extensibility.Interfaces;
+
+namespace BDK.XrmToolBox.StorageCleaner
+{
+    public partial class PluginControl : PluginControlBase, IGitHubPlugin, IPayPalPlugin
+    {
+        public string RepositoryName
+        {
+            get
+            {
+                return "XrmToolBoxPlugins";
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return "deepakkumar1984";
+            }
+        }
+
+        public string DonationDescription
+        {
+            get
+            {
+                return "Please help us improve this tool!";
+            }
+        }
+
+        public string EmailAccount
+        {
+            get
+            {
+                return "support@appshap.com";
+            }
+        }
+
+        public PluginControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BDK.XrmToolBox/BDK.XrmToolBox.StorageCleaner/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Storage Space Cleaner")]
+[assembly: AssemblyDescription("Check and manage storage usage and clean-up")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Deepak Battini")]
+[assembly: AssemblyProduct("BDK.XrmToolBox.StorageCleaner")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0d801ae5-c241-4c37-8649-c71ca8f95409")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2016.11.1")]
+[assembly: AssemblyFileVersion("1.2016.11.1")]
